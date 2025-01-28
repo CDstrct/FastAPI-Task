09125d82d23243ab281cbbf10cd9aca8dcb0f016
CDstrct <kacper.mitrega@edu.uekat.pl>
2025-01-28 19:16:18 +0100

    Create crud.py

---
+++ app/crud.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    db_task = models.Task(title=task.title, description=task.description)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
